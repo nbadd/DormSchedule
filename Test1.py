2cd20a02055f3880a739eea2043d0f2d524ea543
Nikhil Baddam <nbaddam17@gmail.com>
2017-05-15 14:28:19 -0400

    Test1Complete

--- Test1.py
+++ Test1.py
@@ -2,7 +2,11 @@ import datetime
 from datetime import timedelta
 
 from datetime import date
-d0 = date(2017, 9, 5)
-d1 = d0 + timedelta(days = 7)
-
-print (d1)
+d0 = date(2017, 9, 7)
+year = [d0]
+count = 0
+while count < 39:
+    d0 = d0 + timedelta(days = 7)
+    year.append(d0)
+    count+=1
+print (year)
