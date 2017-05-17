961b03c40789c2fe922fb67710ccb3d2332b8519
RyanNeff <rynoneff@gmail.com>
2017-05-17 15:03:01 -0400

    Sunday is a day too
    Added a Sunday faculty shift, also made everything more efficient and
    clearer to the programmer and user

--- Test1.py
+++ Test1.py
@@ -5,56 +5,38 @@ from datetime import date
 import random
 
 import csv
+#Lists of faculty for each weekday group
+monday = ['Monday']
+tuesday = ['Tuesday']
+wednesday = ['Wednesday']
+thursday = ['Thursday']
+friday = ['Friday']
+saturday = ['Saturday']
+sunday = ['Sunday']
+year = []
+
 testFaculty = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,bb,cc,dd,ee,ff,gg,hh,ii,jj,kk,ll,mm,nn'
+faculty = testFaculty.split(',') #Create list of faculty for testing
 
-dM = date(2017, 9, 11)
-dT = date(2017, 9, 5)
-dW = date(2017, 9, 6)
-dTh = date(2017, 9, 7)
-dF = date(2017, 9, 8)
-dSat = date(2017, 9, 9)
-dSun = date(2017, 9, 10)
-year = [dT,dW,dTh,dF,dSat,dSun,dM]
-count = 0
-while count < 39:
-    dT = dT + timedelta(days = 7)
-    year.append(dT)
-    dW = dW + timedelta(days = 7)
-    year.append(dW)
-    dTh = dTh + timedelta(days = 7)
-    year.append(dTh)
-    dF = dF + timedelta(days = 7)
-    year.append(dF)
-    dSat = dSat + timedelta(days = 7)
-    year.append(dSat)
-    dSun = dSun + timedelta(days = 7)
-    year.append(dSun)
-    dM = dM + timedelta(days = 7)
-    year.append(dM)
-    count+=1
+firstDay = input('Input the integer day of the first Sunday of the school year:')
 
-monday = []
-tuesday = []
-wednesday = []
-thursday = []
-friday = []
-saturday = []
-sunday = []
-weekend = []
-faculty = []
-floater = []
-count = 0
-faculty = testFaculty.split(',')
+d0 = date(2017, 9, int(firstDay)) #First Sunday of the year
+year = [d0 + timedelta(days=d) for d in range(40*7)]
 
-for x in range(8):
-    floater.append(str(x))
+floater = [str(x) for x in range(8)] #Create floater list for testing
+floater.insert(0,'Floater')
 
 # print('Input list of faculty separated by spaces:')
 # f = input()
 # faculty = f.split(' ')
 # print(faculty)
 
+#Assign the random faculty groups
 for x in range(8):
+    count = random.randint(0,len(faculty)-1)
+    sunday.append(faculty[count])
+    faculty.remove(faculty[count])
+
     count = random.randint(0,len(faculty)-1)
     monday.append(faculty[count])
     faculty.remove(faculty[count])
@@ -71,25 +53,34 @@ for x in range(8):
     thursday.append(faculty[count])
     faculty.remove(faculty[count])
 
-
-#print(monday, tuesday, wednesday, thursday)
-
-
-
-#print(year)tuesday.insert(0,year[1])mondaytuesday
-week = [monday,tuesday,wednesday,thursday, friday, saturday, sunday]
+week = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
 countyear = 0
+countday = 0
 countweek = 0
-countweekend = 0
 with open('Schedule.csv', 'w') as f:
     writer = csv.writer(f)
-    while (countyear < len(year)):
-        countweek = 0
-        while (countweek < len(week)):
-            week[countweek].insert(0,year[countyear])
-            writer.writerow(week[countweek])
-            week[countweek].remove(week[countweek][0])
-            countyear += 1
-            countweek += 1
-    year[5 + countweekend]
+    while (countyear < len(year)): #Years
+        countweek = 6
+        #while (countweek > 0): #Weeks
+        for x in range(6):
+            countday = 0
+            while (countday < len(week) and countyear < len(year)): #Days
+                if (countday == 6):
+                    week[countday].insert(0,year[countyear])
+                    if (countweek == 1):
+                        week[countday].extend(floater)
+                        writer.writerow(week[countday])
+                    else:
+                        week[countday].extend(week[countday - countweek])
+                        writer.writerow(week[countday])
+
+                    week[countday] = ['Saturday']
+                    countweek -= 1
+                else:
+                    week[countday].insert(0,year[countyear])
+                    writer.writerow(week[countday])
+                    week[countday].remove(week[countday][0])
+
+                countyear += 1
+                countday += 1
 f.close()
