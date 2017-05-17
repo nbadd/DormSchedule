3d34b16d7fb5b141a613f4b5ee44ab837366fdfe
RyanNeff <rynoneff@gmail.com>
2017-05-17 16:38:01 -0400

    Now with extra substitutions!

--- Test1.py
+++ Test1.py
@@ -5,15 +5,15 @@ from datetime import date
 import random
 
 import csv
+
 #Lists of faculty for each weekday group
-monday = ['Monday']
-tuesday = ['Tuesday']
-wednesday = ['Wednesday']
-thursday = ['Thursday']
-friday = ['Friday']
-saturday = ['Saturday']
-sunday = ['Sunday']
-year = []
+monday = []#'Monday']
+tuesday = []#'Tuesday']
+wednesday = []#'Wednesday']
+thursday = []#'Thursday']
+friday = []#'Friday']
+saturday = []#'Saturday']
+sunday = []#'Sunday']
 
 testFaculty = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,bb,cc,dd,ee,ff,gg,hh,ii,jj,kk,ll,mm,nn'
 faculty = testFaculty.split(',') #Create list of faculty for testing
@@ -24,7 +24,7 @@ d0 = date(2017, 9, int(firstDay)) #First Sunday of the year
 year = [d0 + timedelta(days=d) for d in range(40*7)]
 
 floater = [str(x) for x in range(8)] #Create floater list for testing
-floater.insert(0,'Floater')
+#floater.insert(0,'Floater')
 
 # print('Input list of faculty separated by spaces:')
 # f = input()
@@ -54,28 +54,31 @@ for x in range(8):
     faculty.remove(faculty[count])
 
 week = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
+
 countyear = 0
-countday = 0
-countweek = 0
 with open('Schedule.csv', 'w') as f:
     writer = csv.writer(f)
     while (countyear < len(year)): #Years
-        countweek = 6
+        countweek = 0
         #while (countweek > 0): #Weeks
         for x in range(6):
             countday = 0
             while (countday < len(week) and countyear < len(year)): #Days
                 if (countday == 6):
                     week[countday].insert(0,year[countyear])
-                    if (countweek == 1):
+                    if (countweek == 5):
                         week[countday].extend(floater)
                         writer.writerow(week[countday])
                     else:
-                        week[countday].extend(week[countday - countweek])
+                        week[countday].extend(week[countweek])
                         writer.writerow(week[countday])
 
-                    week[countday] = ['Saturday']
-                    countweek -= 1
+                    week[countday] = []#'Saturday']
+                    countweek += 1
+                elif (countday == countweek and countday != 5):
+                    floater.insert(0,year[countyear])
+                    writer.writerow(floater)
+                    floater.remove(floater[0])
                 else:
                     week[countday].insert(0,year[countyear])
                     writer.writerow(week[countday])
